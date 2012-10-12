91885834d100422f61deba9c7b666e443ce9e5b3
byantos <adk3000@q.com>
2012-10-12 00:00:05 -0600

    Implemented the missing stat events

--- src/com/deaboy/whois/EventListener.java
+++ src/com/deaboy/whois/EventListener.java
@@ -8,15 +8,20 @@ import java.util.logging.Level;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.Material;
+import org.bukkit.entity.Arrow;
 import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.HandlerList;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
+import org.bukkit.event.entity.EntityDamageByEntityEvent;
+import org.bukkit.event.entity.EntityDamageEvent;
+import org.bukkit.event.entity.EntityDamageEvent.DamageCause;
+import org.bukkit.event.entity.EntityDeathEvent;
+import org.bukkit.event.entity.PlayerDeathEvent;
 import org.bukkit.event.player.PlayerInteractEntityEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
-import org.bukkit.event.player.PlayerKickEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
 
 import com.deaboy.whois.users.User;
@@ -99,7 +104,7 @@ public class EventListener implements Listener, Closeable
 			
 			if (!u.getField(Field.REAL_NAME).isEmpty())
 			{
-				message += "(" + u.getField(Field.REAL_NAME) + ")";
+				message += " (" + u.getField(Field.REAL_NAME) + ")";
 			}
 			
 			message += " has left the game.";
@@ -107,35 +112,6 @@ public class EventListener implements Listener, Closeable
 			e.setQuitMessage(message);
 			
 			Whois.unloadUser(e.getPlayer());
-		}
-	}
-	
-	public void onPlayerKick(PlayerKickEvent e)
-	{
-		if (!UserFile.exists(e.getPlayer().getName()) && Whois.getSettings().getBoolean("whitelist", false))
-		{
-			e.setLeaveMessage(null);
-			return;
-		}
-		else
-		{
-			User u = new User(e.getPlayer());
-			
-			String message = new String();
-			
-			message += ChatColor.YELLOW;
-			message += e.getPlayer().getName();
-			
-			if (!u.getField(Field.REAL_NAME).isEmpty())
-			{
-				message += "(" + u.getField(Field.REAL_NAME) + ")";
-			}
-			
-			message += " has left the game.";
-			
-			e.setLeaveMessage(message);
-			
-			u.close();
 		}
 	}
 	
@@ -147,7 +123,7 @@ public class EventListener implements Listener, Closeable
 			return;
 		}
 		
-		if (e.getRightClicked().getType() == EntityType.PLAYER)
+		if (e.getRightClicked().getType() == EntityType.PLAYER && e.getPlayer().getItemInHand().getType() == Material.STICK)
 		{
 			Commands.sendWhoisInfo(e.getPlayer(), ((Player) e.getRightClicked()).getName());
 		}
@@ -163,14 +139,148 @@ public class EventListener implements Listener, Closeable
 		
 		User u = Whois.getUser(e.getPlayer());
 		
-		if (e.getBlock().getType().equals(Material.DIAMOND_ORE))
+		if (e.getBlock().getType() == Material.DIAMOND_ORE)
 		{
 			u.incrementStat(Stat.DIAMONDS);
 		}
 	}
 	
+	@EventHandler
+	public void onPlayerDeath(PlayerDeathEvent e)
+	{
+		if (!Whois.getSettings().getBoolean("track_stats", true) )
+		{
+			return;
+		}
+		
+		User u = Whois.getUser(e.getEntity());
+		
+		u.incrementStat(Stat.DEATH_COUNT);
+		
+	}
+	
+	@EventHandler
+	public void onPlayerDamage(EntityDamageEvent e)
+	{
+		if (!Whois.getSettings().getBoolean("track_stats", true) || e.isCancelled() || e.getEntityType() != EntityType.PLAYER)
+		{
+			return;
+		}
+		
+		User u = Whois.getUser((Player)e.getEntity());
+		
+		u.incrementStat(Stat.DAMAGE_TAKEN, (long)e.getDamage());
+		
+	}
+	
+	@EventHandler
+	public void onEntityDeath(EntityDeathEvent e)
+	{
+		if ( !Whois.getSettings().getBoolean("track_stats", true))
+		{
+			return;
+		}
+		User u;
+		if (e.getEntity().getLastDamageCause().getCause() == DamageCause.ENTITY_ATTACK)
+		{
+			if (((EntityDamageByEntityEvent) e.getEntity().getLastDamageCause()).getDamager().getType() == EntityType.PLAYER)
+			{
+				u = Whois.getUser((Player) ((EntityDamageByEntityEvent) e.getEntity().getLastDamageCause()).getDamager());
+			}
+			else if (((EntityDamageByEntityEvent) e.getEntity().getLastDamageCause()).getDamager().getType() == EntityType.ARROW
+					&& ((Arrow) ((EntityDamageByEntityEvent) e.getEntity().getLastDamageCause()).getDamager()).getShooter().getType() == EntityType.PLAYER)
+			{
+				u = Whois.getUser((Player) ((Arrow) ((EntityDamageByEntityEvent) e.getEntity().getLastDamageCause()).getDamager()).getShooter());
+			}
+			else
+			{
+				return;
+			}
+		}
+		else
+		{
+			return;
+		}
+		
+		if ( isHostile(e.getEntityType()) )
+		{
+			u.incrementStat(Stat.KILLS_HOSTILE);
+		}
+		else if ( isPassive(e.getEntityType()))
+		{
+			u.incrementStat(Stat.KILLS_PASSIVE);
+		}
+		else if ( e.getEntityType() == EntityType.PLAYER)
+		{
+			u.incrementStat(Stat.KILLS_PLAYERS);
+		}
+		else
+		{
+			return;
+		}
+	}
+	
+	@EventHandler
+	public void onEntityDamageByEntity( EntityDamageByEntityEvent e)
+	{
+		if ( !Whois.getSettings().getBoolean("track_stats", true))
+		{
+			return;
+		}
+		User u;
+		if (e.getDamager().getType() == EntityType.PLAYER)
+		{
+			u = Whois.getUser((Player) e.getDamager());
+		}
+		else if ( e.getDamager().getType() == EntityType.ARROW
+				&& ((Arrow) e.getDamager()).getShooter().getType() == EntityType.PLAYER)
+		{
+			u = Whois.getUser((Player) ((Arrow) e.getDamager()).getShooter());
+		}
+		else
+		{
+			return;
+		}
+
+		
+		u.incrementStat(Stat.DAMAGE_DEALT, (long)e.getDamage());
+	}
+	
 	public void close()
 	{
 		HandlerList.unregisterAll(this);
+	}
+	
+	public static boolean isHostile(EntityType type)
+	{
+		
+
+		return (type == EntityType.ZOMBIE
+				|| type == EntityType.SKELETON
+				|| type == EntityType.SPIDER
+				|| type == EntityType.CREEPER
+				|| type == EntityType.SLIME
+				|| type == EntityType.ENDERMAN
+				|| type == EntityType.CAVE_SPIDER
+				|| type == EntityType.SILVERFISH
+				|| type == EntityType.PIG_ZOMBIE
+				|| type == EntityType.GHAST
+				|| type == EntityType.MAGMA_CUBE
+				|| type == EntityType.BLAZE
+				|| type == EntityType.ENDER_DRAGON);
+	}
+
+	public static boolean isPassive(EntityType type)
+	{
+
+		return (type == EntityType.PIG
+				|| type == EntityType.COW
+				|| type == EntityType.SHEEP
+				|| type == EntityType.CHICKEN
+				|| type == EntityType.SQUID
+				|| type == EntityType.WOLF
+				|| type == EntityType.OCELOT
+				|| type == EntityType.MUSHROOM_COW
+				|| type == EntityType.VILLAGER);
 	}
 }
